0c6111700f19379d8b030de45e0d4597dea5ea16
DevMinGeonPark <devmin7985@gmail.com>
2022-06-23 00:19:10 +0900

    temporary

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,23 @@
-
 require("dotenv").config();
 
+// MYSQL_HOST = "118.67.133.207"
+// MYSQL_USER = "ecofit"
+// MYSQL_PASSWORD = "ecofit12"
+// MYSQL_DATABASE = "ecofit"
+// MYSQL_PORT = "3306"
+
 module.exports = {
-    host : 'localhost',
-    user : 'root',
+    host : process.env.MYSQL_HOST,
+    user : process.env.MYSQL_USER,
     password : process.env.MYSQL_PASSWORD,
-    database : 'echo_fit',
-    port : '3306'
+    database : process.env.MYSQL_DATABASE,
+    port : process.env.MYSQL_PORT
 };
+
+// module.exports = {
+//     host : "118.67.133.207",
+//     user : "ecofit",
+//     password : "ecofit12",
+//     database : "ecofit",
+//     port : "3306"
+// };
